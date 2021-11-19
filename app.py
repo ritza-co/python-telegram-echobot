570d049ddf2382dcbef741a595a07d736dab1938
AnesuMurakata <murac96@gmail.com>
2021-11-19 16:01:16 +0200

    Using webhooks

--- app.py
+++ app.py
@@ -1,62 +1,64 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import re
+from flask import Flask, request
+import telegram
 from flask import Flask, request
 import os
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+TOKEN="2131425995:AAFAOT9xgDM00FRMI9lyA7SvefK7LbS__40"
 
-logger = logging.getLogger(__name__)
+bot = telegram.Bot(token=TOKEN)
 
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+   # retrieve the message in JSON and then transform it to Telegram object
+   update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+   chat_id = update.message.chat.id
+   msg_id = update.message.message_id
+
+   # Telegram understands UTF-8, so encode text for unicode compatibility
+   text = update.message.text.encode('utf-8').decode()
+   # for debugging purposes only
+   print("got text message :", text)
+   # the first time you chat with the bot AKA the welcoming message
+   if text == "/start":
+       # print the welcoming message
+       bot_welcome = """
+       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       """
+       # send the welcoming message
+       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+   else:
+       try:
+           # clear the message we got from any non alphabets
+           text = re.sub(r"\W", "_", text)
+           # create the api link for the avatar based on http://avatars.adorable.io/
+           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+           # reply with a photo to the name the user sent,
+           # note that you can send photos by url and telegram will fetch it for you
+           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+       except Exception:
+           # if things went wrong
+           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
 
+   return 'ok'
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+   if s:
+       return "webhook setup ok"
+   else:
+       return "webhook setup failed"
 
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(os.getenv('BOT_TOKEN'), use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+@app.route('/')
+def index():
+   return '.'
 
 
 if __name__ == '__main__':
-    main()
+   app.run(threaded=True)
