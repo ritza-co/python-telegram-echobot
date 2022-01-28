a39fb23e5e8cd5e2f460ec5f6a73f1e144e44032
AnesuMurakata <murac96@gmail.com>
2022-01-28 13:02:10 +0200

    Added echo functionality

--- app.py
+++ app.py
@@ -1,51 +1,31 @@
-# import everything
 from flask import Flask, request
 import telegram
 import os
-#from telebot.credentials import bot_token, bot_user_name,URL
-#global bot
-#global TOKEN
 TOKEN = os.getenv('BOT_TOKEN')
 URL = os.getenv('URL')
 bot = telegram.Bot(token=TOKEN)
 
-# start the flask app
 app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
    # retrieve the message in JSON and then transform it to Telegram object
    update = telegram.Update.de_json(request.get_json(force=True), bot)
-
    chat_id = update.message.chat.id
    msg_id = update.message.message_id
 
    # Telegram understands UTF-8, so encode text for unicode compatibility
    text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
+  
    # the first time you chat with the bot AKA the welcoming message
    if text == "/start":
        # print the welcoming message
-       bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-       """
+       bot_welcome = "Hi! I respond by echoing messages. Give it a try!"
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-
    else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+       bot.sendMessage(chat_id=chat_id, text=text, reply_to_message_id=msg_id)
 
    return 'ok'
 
