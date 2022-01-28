e2b52c9971c96e5df1384bde94bea0146527ed99
AnesuMurakata <murac96@gmail.com>
2022-01-28 12:47:05 +0200

    New version

--- app.py
+++ app.py
@@ -1,32 +1,69 @@
-import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+# import everything
+from flask import Flask, request
+import telegram
+#from telebot.credentials import bot_token, bot_user_name,URL
+#global bot
+#global TOKEN
+TOKEN = os.getenv('BOT_TOKEN')
+URL = os.getenv('URL')
+bot = telegram.Bot(token=TOKEN)
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I respond by echoing messages. Give it a try!")
+# start the flask app
+app = Flask(__name__)
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+   # retrieve the message in JSON and then transform it to Telegram object
+   update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-def main():
-    TOKEN = os.getenv('BOT_TOKEN')
-    #URL = os.getenv('URL')
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+   chat_id = update.message.chat.id
+   msg_id = update.message.message_id
 
-    start_handler = CommandHandler("start", start)
+   # Telegram understands UTF-8, so encode text for unicode compatibility
+   text = update.message.text.encode('utf-8').decode()
+   # for debugging purposes only
+   print("got text message :", text)
+   # the first time you chat with the bot AKA the welcoming message
+   if text == "/start":
+       # print the welcoming message
+       bot_welcome = """
+       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       """
+       # send the welcoming message
+       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-    dispatcher.add_handler(start_handler)
 
-    # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+   else:
+       try:
+           # clear the message we got from any non alphabets
+           text = re.sub(r"\W", "_", text)
+           # create the api link for the avatar based on http://avatars.adorable.io/
+           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+           # reply with a photo to the name the user sent,
+           # note that you can send photos by url and telegram will fetch it for you
+           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+       except Exception:
+           # if things went wrong
+           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
 
-    #updater.start_polling()
+   return 'ok'
 
-    PORT = int(os.environ.get('PORT'))
-    HOOK_URL = 'https://python-telegram-echobot-zehdpu.codecapsules.co.za' + '/' + TOKEN
-    updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN, webhook_url=HOOK_URL)
-    updater.idle()
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    # we use the bot object to link the bot to our app which live
+    # in the link provided by URL
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    # something to let us know things work
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/')
+def index():
+    return '.'
 
 if __name__ == '__main__':
-    main()
+    # note the threaded arg which allow
+    # your app to have more than one thread
+    app.run(threaded=True)
